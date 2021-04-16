6b217c518c310ca1fd9ccb00454d7276a1e86c60
Sachin Jambur <sachin.jambur@neofinancial.com>
2021-04-16 09:02:11 -0600

    Initial Cl Commit

---
+++ app/utility.ts
@@ -0,0 +1,10 @@
+function getInputValue(elementID: string): string {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    return inputElement.value;
+}
+
+function logger(message: string): void{
+    console.log(message);
+}
+
+export { getInputValue as getValue, logger };
